1844c1bb75e0f5a3860d09632a013566b822569f
anne04 <anne.06.cse@gmail.com>
2023-12-01 16:43:21 -0500

    Update run_Seurat.R

--- run_Seurat.R
+++ run_Seurat.R
@@ -19,16 +19,49 @@ for(i in 1:ncol(df)) {
   cell_y[[i]] <- df[ , i]    
 }
 
-countsData <- read.csv(file = paste('/cluster/home/t116508uhn/synthetic_gene_vs_cell_',options,'.csv', sep=""),row.names = 1) # read.csv(file = '/cluster/home/t116508uhn/synthetic_gene_vs_cell_type6_f.csv',row.names = 1)
-pdac_sample <- CreateSeuratObject(counts = countsData)
+coord_x <- read.csv('/mnt/data1/fatema/x_coord_mgDF.csv', header = FALSE)
+coord_x_vector <- coord_x[, 1]
+coord_y <- read.csv('/mnt/data1/fatema/y_coord_mgDF.csv', header = FALSE)
+coord_y_vector <- coord_y [, 1]
 
 
-temp <- SCTransform(pdac_sample)
-#temp <- ScaleData(pdac_sample)
-#temp <- FindVariableFeatures(temp) 
+
+
+
+
+data1_path = '/mnt/data1/fatema/IMC_T1D_data1/'
+countsData <- read.csv(file = paste('/mnt/data1/fatema/IMC_T1D_data1/gene_vs_cell_mgDF.csv', sep=""), row.names = 1) 
+
+####################################################################################
+mat <- np$load("/mnt/data1/fatema/IMC_T1D_data1/gene_vs_cell_count_mgDF.npy")
+cell_barcodes <- read.csv('/mnt/data1/fatema/IMC_T1D_data1/cell_id_mgDF.csv', header = FALSE)
+# nrow(cell_barcodes) is 1406 and ncol(cell_barcodes) is 1.
+cell_vector <- cell_barcodes[, 1]
+gene_ids <- read.csv('/mnt/data1/fatema/IMC_T1D_data1/protein_marker_mgDF.csv', header = FALSE)
+# nrow(gene_ids) is 19523 and ncol(gene_ids) is 1.
+gene_vector <- gene_ids[, 1]
+colnames(mat) <- cell_vector
+rownames(mat) <- gene_vector
+
+mat@meta.data$x <- coord_x_vector
+mat@meta.data$y <- coord_y_vector
+
+
+countsData <- mat
+#####################################################################################
+temp <- CreateSeuratObject(counts = countsData)
+temp <- SCTransform(temp)
+temp <- NormalizeData(temp)
+temp <- FindVariableFeatures(temp) 
+temp <- ScaleData(temp)
+
 temp <- RunPCA(temp, verbose = FALSE)
 temp <- FindNeighbors(temp, reduction = "pca", dims = 1:30)
-temp <- FindClusters(temp, verbose = FALSE)
+temp <- FindClusters(temp)
+save(temp, file='/mnt/data1/fatema/IMC_T1D_data1/temp.Rda')
+temp_matrix <- temp[['seurat_clusters']]
+write.csv(temp_matrix, '/mnt/data1/fatema/IMC_T1D_data1/seurat_clusters.csv')
+
 temp <- RunUMAP(temp , reduction = "pca", dims = 1:30)
 p1 <- DimPlot(temp, reduction = "umap",group.by = 'seurat_clusters', label = TRUE)
 p2 <- SpatialDimPlot(temp, label = TRUE,group.by = 'seurat_clusters', label.size = 3)
