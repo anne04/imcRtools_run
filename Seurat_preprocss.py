3210fd7f734d8eab59a65fdb18406b2aa550f01f
anne04 <anne.06.cse@gmail.com>
2023-12-01 14:45:00 -0500

    Update Seurat_preprocss.py

--- Seurat_preprocss.py
+++ Seurat_preprocss.py
@@ -7,6 +7,8 @@ import sys
 from collections import defaultdict
 data1_path = '/mnt/data1/fatema/IMC_T1D_data1/'
 
+
+
 df = pd.read_csv('/mnt/data1/fatema/IMC_T1D/raw_data/mgDF.csv', sep=",", header=0,index_col=0) 
 # seperate the protein names, (x, y), and cell names
 
@@ -31,7 +33,7 @@ for j in range (0, len(cell_name)):
     cell = cell_name[j]
     file_name.append(df['TIFFfilename'][cell])
     status_list.append(df['Status'][cell])
-    cell_key = str(j+1)+'-'+file_name[j] + '-' + status_list[j]
+'''    cell_key = str(j+1)+'-'+file_name[j] + '-' + status_list[j]
     if cell_key in label_dictionary:   
         # mark all the greek letter cell types as 'Islet_Cells'
         label = ''
@@ -47,13 +49,7 @@ for j in range (0, len(cell_name)):
 
 print('unique cell label: %d'%len(set(cell_label)))
 print(set(cell_label))
-
-gene_vs_cell = np.zeros((len(protein_name),len(cell_name)))
-for i in range (0, len(protein_name)):
-    gene = protein_name[i]
-    for j in range (0, len(cell_name)):
-        cell = cell_name[j]
-        gene_vs_cell[i][j] = df[gene][cell]
+'''
 
 ###########
 data_list=defaultdict(list)
@@ -90,6 +86,6 @@ df.to_csv(data1_path+'x_coord_mgDF.csv', index=False, header=False)
 df = pd.DataFrame(y_coord)
 df.to_csv(data1_path+'y_coord_mgDF.csv', index=False, header=False)
 
-df = pd.DataFrame(cell_label)
-df.to_csv(data1_path+'cell_label_islets_mgDF.csv', index=False, header=False)
+#df = pd.DataFrame(cell_label)
+#df.to_csv(data1_path+'cell_label_islets_mgDF.csv', index=False, header=False)
 
