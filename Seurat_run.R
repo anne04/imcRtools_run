368efc5ff5e099c9960eb12ed16f90e75a604e8a
anne04 <anne.06.cse@gmail.com>
2023-12-07 15:23:13 -0500

    Update Seurat_run.R

--- Seurat_run.R
+++ Seurat_run.R
@@ -16,8 +16,6 @@ gene_ids <- read.csv('/mnt/data1/fatema/IMC_T1D_data1/protein_marker_mgDF.csv', 
 gene_vector <- gene_ids[, 1]
 colnames(mat) <- cell_vector
 rownames(mat) <- gene_vector
-#mat@meta.data$x <- coord_x_vector
-#mat@meta.data$y <- coord_y_vector
 countsData <- mat
 #####################################################################################
 temp <- CreateSeuratObject(counts = countsData)
@@ -62,7 +60,7 @@ temp <- ScaleData(temp)
 
 temp <- RunPCA(temp, verbose = FALSE)
 temp <- FindNeighbors(temp, reduction = "pca", dims = 1:30)
-temp <- FindClusters(temp) #, resolution=0.3
+temp <- FindClusters(temp, resolution=0.1) #
 save(temp, file='/mnt/data1/fatema/IMC_mgdf_gad+_gad-sample/mgDF_GAD+GAD-temp.Rda')
 temp_matrix <- temp[['seurat_clusters']]
 write.csv(temp_matrix, '/mnt/data1/fatema/IMC_mgdf_gad+_gad-sample/seurat_clusters_mgDF_GAD+GAD-.csv')
@@ -94,7 +92,7 @@ temp <- ScaleData(temp)
 
 temp <- RunPCA(temp, verbose = FALSE)
 temp <- FindNeighbors(temp, reduction = "pca", dims = 1:30)
-temp <- FindClusters(temp, resolution=0.3)
+temp <- FindClusters(temp, resolution=0.1)
 save(temp, file='/mnt/data1/fatema/IMC_T1Dsample/T1D-temp.Rda')
 temp_matrix <- temp[['seurat_clusters']]
 write.csv(temp_matrix, '/mnt/data1/fatema/IMC_T1Dsample/seurat_clusters_T1D.csv')
@@ -104,17 +102,3 @@ temp_protein_cell <- as.matrix(temp_protein_cell)
 write.csv(temp_protein_cell, paste('/mnt/data1/fatema/IMC_T1Dsample/normalized_protein_T1D.csv',sep=""))
 
 #################################################################################
-
-
-
-
-
-
-
-
-
-
-temp <- RunUMAP(temp , reduction = "pca", dims = 1:30)
-p1 <- DimPlot(temp, reduction = "umap",group.by = 'seurat_clusters', label = TRUE)
-p2 <- SpatialDimPlot(temp, label = TRUE,group.by = 'seurat_clusters', label.size = 3)
-ggsave("/cluster/home/t116508uhn/64630/myplot.png", plot = (p1+p2))
