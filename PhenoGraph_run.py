d1dc9522dc4db54991d0b00315996d710cdd59a3
anne04 <anne.06.cse@gmail.com>
2023-12-07 15:22:10 -0500

    Update PhenoGraph_run.py

--- PhenoGraph_run.py
+++ PhenoGraph_run.py
@@ -1,21 +1,25 @@
 import phenograph
 import numpy as np
-
+import pandas as pd
 
 data1_path = '/mnt/data1/fatema/IMC_T1D_data1/'
 data2_path = '/mnt/data1/fatema/IMC_mgdf_gad+_gad-sample/'
 type = ['mgDF', 'mgDF_GAD+GAD-', 'T1D']
 type_id = 0
 
-#data1_path_from = '/mnt/data1/fatema/IMC_T1D/raw_data/mgDF.csv'
-#data2_path_from = '/mnt/data1/fatema/IMC_T1D/raw_data/mgDF_GAD+GAD-samples.csv'
-
-
-
 # data should be cell vs gene ndarray
-data = np.load(data1_path + "gene_vs_cell_count_"+type[type_id], gene_vs_cell)
+data = np.load(data1_path + "gene_vs_cell_count_"+type[type_id]+".npy")
 print(data.shape)
-data.transpose()
+data = data.transpose()
 print(data.shape)
 
-communities, graph, Q = phenograph.cluster(data)
+df_normalized_protein = pd.read_csv('/mnt/data1/fatema/IMC_T1D_data1/normalized_protein.csv', sep=",", header=0,index_col=0) 
+df_normalized_protein = df_normalized_protein.T # now rows are cells and columns are proteins
+
+# remove first row and first column
+df_normalized_protein.drop(index=df.index[0], axis=0, inplace=True)
+df_normalized_protein.pop('column-name')
+data = df_normalized_protein.to_numpy()
+
+
+communities, graph, Q = phenograph.cluster(data) #higher k = less number of clusters. Default k=30
