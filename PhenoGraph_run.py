d1f25a49033092da0885c5efef1e6089077119c3
anne04 <anne.06.cse@gmail.com>
2023-12-11 10:35:41 -0500

    Update PhenoGraph_run.py

--- PhenoGraph_run.py
+++ PhenoGraph_run.py
@@ -4,7 +4,11 @@ import pandas as pd
 
 data1_path = '/mnt/data1/fatema/IMC_T1D_data1/'
 data2_path = '/mnt/data1/fatema/IMC_mgdf_gad+_gad-sample/'
+data3_path= '/mnt/data1/fatema/IMC_T1Dsample/'
 type = ['mgDF', 'mgDF_GAD+GAD-', 'T1D']
+
+
+##############################################################################
 type_id = 0
 
 # data should be cell vs gene ndarray
@@ -14,12 +18,27 @@ data = data.transpose()
 print(data.shape)
 
 df_normalized_protein = pd.read_csv('/mnt/data1/fatema/IMC_T1D_data1/normalized_protein.csv', sep=",", header=0,index_col=0) 
-df_normalized_protein = df_normalized_protein.T # now rows are cells and columns are proteins
+data = df_normalized_protein.values
+communities, graph, Q = phenograph.cluster(data) #higher k = less number of clusters. Default k=30
 
+
+#####################################################################################
+
+type_id = 2
+# data should be cell vs gene ndarray
+df_normalized_protein = pd.read_csv(data3_path+'normalized_protein_'+ type[type_id] +'.csv', sep=",", header=0,index_col=0) 
+#df_normalized_protein = df_normalized_protein.T # now rows are cells and columns are proteins
 # remove first row and first column
-df_normalized_protein.drop(index=df.index[0], axis=0, inplace=True)
-df_normalized_protein.pop('column-name')
-data = df_normalized_protein.to_numpy()
+data = df_normalized_protein.values
+print(data.shape)
+communities, graph, Q = phenograph.cluster(data) #higher k = less number of clusters. Default k=30
 
-
+######################################################################################
+type_id = 1
+# data should be cell vs gene ndarray
+df_normalized_protein = pd.read_csv(data2_path+'normalized_protein_'+ type[type_id] +'.csv', sep=",", header=0,index_col=0) 
+#df_normalized_protein = df_normalized_protein.T # now rows are cells and columns are proteins
+# remove first row and first column
+data = df_normalized_protein.values
+print(data.shape)
 communities, graph, Q = phenograph.cluster(data) #higher k = less number of clusters. Default k=30
