a1ba18e7d3176838e69465970614029ef0f6b463
anne04 <anne.06.cse@gmail.com>
2023-12-12 15:06:09 -0500

    Update PhenoGraph_postprocess.py

--- PhenoGraph_postprocess.py
+++ PhenoGraph_postprocess.py
@@ -17,12 +17,19 @@ type_id = 0
 df_normalized_protein = pd.read_csv(data1_path_to+'normalized_protein.csv', sep=",", header=0,index_col=0) 
 df_normalized_protein = df_normalized_protein.T # now rows are cells and columns are proteins
 
-clusters = np.load(data1_path_to+'phenograph_clusters_'+type[type_id]+'.npy')
+clusters = np.load(data1_path_to+'phenograph_clusters_'+type[type_id]+'.npy')  # ? clusters with k = 1000
 num_cells = clusters.shape[0]
 cluster_id = []
 for i in range (0, num_cells):
     cluster_id.append(df_clusters[i])
-    
+
+
+df = pd.read_csv('/mnt/data1/fatema/seurat_clusters/mgDF_data/seurat_clusters_mgDF.csv', sep=",", header=0,index_col=0) # 22 clusters with resolution = 0.2
+df['seurat_clusters']=cluster_id
+df = df.rename(columns={'seurat_clusters': 'phenograph_clusters'})
+df.to_csv('/mnt/data1/fatema/phenograph_clusters/mgDF_data/'+'phenograph_clusters_'+type[type_id]+'.csv')
+
+
 df_normalized_protein['label']=cluster_id   
 df_normalized_protein.to_csv(data1_path_to+'phenograph_cluster_label_'+type[type_id]+'.csv')
 
@@ -31,12 +38,17 @@ type_id = 1
 df_normalized_protein = pd.read_csv(data2_path_to+'normalized_protein.csv', sep=",", header=0,index_col=0) 
 df_normalized_protein = df_normalized_protein.T # now rows are cells and columns are proteins
 
-clusters = np.load(data2_path_to+'phenograph_clusters_'+type[type_id]+'.npy')
+clusters = np.load(data2_path_to+'phenograph_clusters_less_'+type[type_id]+'.npy') # 23 with k =1000.
 num_cells = clusters.shape[0]
 cluster_id = []
 for i in range (0, num_cells):
-    cluster_id.append(df_clusters[i])
-    
+    cluster_id.append(clusters[i])
+
+df = pd.read_csv('/mnt/data1/fatema/seurat_clusters/mgDF_GAD+-_data/seurat_clusters_mgDF_GAD+GAD-.csv', sep=",", header=0,index_col=0) # 19 clusters with resolution-0.2
+df['seurat_clusters']=cluster_id
+df = df.rename(columns={'seurat_clusters': 'phenograph_clusters'})
+df.to_csv('/mnt/data1/fatema/phenograph_clusters/mgDF_GAD+-_data/'+'phenograph_clusters_'+type[type_id]+'.csv')
+
 df_normalized_protein['label']=cluster_id   
 df_normalized_protein.to_csv(data2_path_to+'phenograph_cluster_label_'+type[type_id]+'.csv')
 ######################################################################################
@@ -45,11 +57,18 @@ type_id = 2
 df_normalized_protein = pd.read_csv(data3_path_to+'normalized_protein.csv', sep=",", header=0,index_col=0) 
 df_normalized_protein = df_normalized_protein.T # now rows are cells and columns are proteins
 
-clusters = np.load(data3_path_to+'phenograph_clusters_'+type[type_id]+'.npy')
+clusters = np.load(data3_path_to+'phenograph_clusters_less_'+type[type_id]+'.npy')  # 21 clusters with k = 1000
 num_cells = clusters.shape[0]
 cluster_id = []
 for i in range (0, num_cells):
-    cluster_id.append(df_clusters[i])
-    
+    cluster_id.append(clusters[i])
+
+
+df = pd.read_csv('/mnt/data1/fatema/seurat_clusters/T1D_data/seurat_clusters_T1D.csv', sep=",", header=0,index_col=0) # 17 clusters with resolution = 0.3
+df['seurat_clusters']=cluster_id
+df = df.rename(columns={'seurat_clusters': 'phenograph_clusters'})
+df.to_csv('/mnt/data1/fatema/phenograph_clusters/T1D_data/'+'phenograph_clusters_'+type[type_id]+'.csv')
+
+
 df_normalized_protein['label']=cluster_id   
 df_normalized_protein.to_csv(data3_path_to+'phenograph_cluster_label_'+type[type_id]+'.csv')
